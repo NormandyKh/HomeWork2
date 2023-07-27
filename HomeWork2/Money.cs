16368b00f0ea29d8b1d7029886594481a56857a8
NormandyKh <khanipov95@gmail.com>
2023-07-27 22:38:37 +0300

    Добавьте файлы проекта.

---
+++ HomeWork2/Money.cs
@@ -0,0 +1,43 @@
+﻿
+namespace HomeWork2
+{
+    internal class Money
+    {
+        private int _dollars;
+        private int _cents;
+
+        //конструктор
+        public Money(int _dollars, int _cents)
+        {
+            // если центов больше чем 100 добавляем доллар и оставшиеся центы
+            this._dollars = _dollars + _cents / 100;
+            this._cents = _cents % 100;
+        }
+
+        //публичное свойство
+        public int Dollars
+        {
+            get { return _dollars; }
+        }
+
+        //публичное свойство
+        public int Cents
+        {
+            get { return _cents; }
+        }
+
+        //метод показывает баланс
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Sum: {_dollars} долларов и {_cents} центо");
+        }
+
+        //метод для добаления денег на счет
+        public void SetBalance(int _dollars, int _cents)
+        {
+            // если центов больше чем 100 добавляем доллар и оставшиеся центы
+            this._dollars = _dollars + _cents / 100;
+            this._cents = _cents % 100;
+        }
+    }
+}
---
+++ HomeWork2/Program.cs
@@ -0,0 +1,8 @@
+﻿using HomeWork2;
+
+Money money = new Money(879, 125);
+
+Product product = new Product("Iphone14", money);
+product.ShowInfo();
+
+product.DecreasePrice(150, 150);
