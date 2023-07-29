376dbee07304f25644b3b134cc94b1e7b5ada671
NormandyKh <khanipov95@gmail.com>
2023-07-29 11:02:39 +0300

    ДЗ по структуре

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -1,8 +1,14 @@
 ﻿using HomeWork2;
 
-Money money = new Money(879, 125);
+//Money money = new Money(879, 125);
 
-Product product = new Product("Iphone14", money);
-product.ShowInfo();
+//Product product = new Product("Iphone14", money);
+//product.ShowInfo();
 
-product.DecreasePrice(150, 150);
+//product.DecreasePrice(150, 150);
+
+DecimalNumber number = new DecimalNumber(10);
+Console.WriteLine("Десяткове число: " + number.DecimalValue);
+Console.WriteLine("Двійкове число: " + number.ToBinary());
+Console.WriteLine("Вісімкове число: " + number.ToOctal());
+Console.WriteLine("Шістнадцяткове число: " + number.ToHexadecimal());
---
+++ HomeWork2/StructHomeWork.cs
@@ -0,0 +1,34 @@
+﻿namespace HomeWork2
+{
+    struct DecimalNumber
+    {
+        private int decimalValue;
+
+        public DecimalNumber(int value)
+        {
+            decimalValue = value;
+        }
+
+        public int DecimalValue
+        {
+            get { return decimalValue; }
+            set { decimalValue = value; }
+        }
+
+        public string ToBinary()
+        {
+            return Convert.ToString(decimalValue, 2);
+        }
+
+        public string ToOctal()
+        {
+            return Convert.ToString(decimalValue, 8);
+        }
+
+        public string ToHexadecimal()
+        {
+            return Convert.ToString(decimalValue, 16).ToUpper();
+        }
+    }
+}
+
